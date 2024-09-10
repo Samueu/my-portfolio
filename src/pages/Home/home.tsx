8f0dccf0a0e2380dab76b3379cbd75ef86fd6425
Samuel Batista <batistasamuel1000@gmail.com>
2024-09-10 15:18:54 -0300

    feat: fixed fonts

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,11 +1,13 @@
 import Navbar from "../../components/Navbar/Navbar";
 import Hero from "./Hero/Hero";
+import Projects from "./Projects/Projects";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Projects />
     </>
   );
 };
